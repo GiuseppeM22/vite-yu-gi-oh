b1704f3040fb4e513fe6eebad55980db9a74c5fd
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-05-23 17:14:24 +0200

    add : base exercise

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+
+import { store } from './data/yugi'
+import axios from 'axios'
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    cardsGen() {
+      axios.get(this.store.api).then((risp) => {
+        const risposta = risp.data.data
+        console.log(risposta)
+        this.store.cards.push(risposta)
+        console.log("mio", this.store.cards)
+      })
+    }
+
+  }
+}
+
+</script>
+
+<template>
+  <pre>{{ store.cards[0] }}</pre>
+
+  <button @click="cardsGen">clicca</button>
+</template>
+
+<style scoped></style>
