9bcf95d890a83b20578dc78131cb11f2dde19826
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-05-23 19:27:37 +0200

    add : all first part

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,32 @@
 <script>
 
 import { store } from './data/yugi'
-import axios from 'axios'
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
 
 export default {
   components: {
-
+    AppHeader,
+    AppMain
   },
   data() {
     return {
       store
     }
   },
-  methods: {
-    cardsGen() {
-      axios.get(this.store.api).then((risp) => {
-        const risposta = risp.data.data
-        console.log(risposta)
-        this.store.cards.push(risposta)
-        console.log("mio", this.store.cards)
-      })
-    }
 
-  }
 }
 
 </script>
 
 <template>
-  <pre>{{ store.cards[0] }}</pre>
-
-  <button @click="cardsGen">clicca</button>
+  <header>
+    <AppHeader />
+  </header>
+  <main>
+    <AppMain />
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,81 @@
+<script>
+
+import { store } from '../data/yugi'
+
+export default {
+    data() {
+        return {
+            store
+        }
+    }
+}
+
+</script>
+
+<template>
+    <section class="bg_orange">
+        <div class="whiteContainer">
+            <h3>Found {{ store.cards.length }} Cards</h3>
+            <div class="cardsContainer">
+                <div v-for="card in store.cards" class="carde">
+                    <img :src="card.card_images[0].image_url" alt="">
+                    <p class="name">{{ card.name }}</p>
+                    <p class="type">{{ card.archetype }}</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.bg_orange {
+    background-color: #d48f38;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+}
+
+.whiteContainer {
+    width: 80%;
+    height: 80%;
+    background-color: white;
+
+}
+
+h3 {
+    background-color: black;
+    color: white;
+    padding: 20px;
+}
+
+.cardsContainer {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.carde {
+    width: calc(100% / 5 - 20px);
+    background-color: #d48f38;
+    margin-bottom: 20px;
+    margin: 10px;
+}
+
+.carde img {
+    max-width: 100%;
+}
+
+.name {
+    text-align: center;
+    color: white;
+    margin-top: 10px;
+}
+
+.type {
+    color: black;
+    margin-top: 10px;
+    text-align: center;
+    margin-bottom: 10px;
+}
+</style>
